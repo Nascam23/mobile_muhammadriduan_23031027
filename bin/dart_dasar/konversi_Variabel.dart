96e2a37b6e4047b6efc9db0289d15599e595316d
duanhtml <parincahan919@gmail.com>
2025-10-22 18:35:52 +0800

    pemr dasar

--- bin/dart_dasar/konversi_Variabel.dart
+++ bin/dart_dasar/konversi_Variabel.dart
@@ -1,14 +1,12 @@
 void main() {
-  // String ke Number
+  
   const inputString = '1000';
   final int inputInt = int.parse(inputString);
   final double inputDouble = double.parse(inputString);
 
-  // Number ke Number lain
   final doubleFromInt = inputInt.toDouble();
   final int intFromDouble = inputDouble.toInt();
 
-  // Number ke String
   final String stringFromInt = inputInt.toString();
   final String stringFromDouble = inputDouble.toString();
   
